3513be20b5ea054e1bb23e698fb48bc8a6a3b46d
wildwarrior_44 <desaivishal4904@gmail.com>
2025-07-25 14:03:32 +0530

    Removed some id's

--- ids.js
+++ ids.js
@@ -2,9 +2,7 @@ export const IDS = [
     // '120363045640332810@g.us', // CWC Community(700)
     // '120363317354530397@g.us', // -> CWC MAIN + ASSIS
     '919322512338@s.whatsapp.net',
-    // '919930056607@s.whatsapp.net',
     '120363055841239377@g.us', // Testing
     // '917517756075@s.whatsapp.net',
     // '917038815102@s.whatsapp.net',
-    // '120363399803703896@g.us' // Siemens Boys
 ]
